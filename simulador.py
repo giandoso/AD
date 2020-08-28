e245f1d340340e06809e1b871c045ffecdf3bf72
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-08-28 17:22:31 -0300

    E[W] correto + little  +  debug print

--- simulador.py
+++ simulador.py
@@ -1,21 +1,25 @@
 from math import log
 from random import random
+import logging, sys
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+# logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
 
 # case 1
 # tempo_medio_clientes, tempo_medio_atendimento = 1.0 / 0.25, 1.0 / 0.15
 
 # case 2
-# tempo_medio_clientes, tempo_medio_atendimento = 1.0 / 2, 1.0 / 1.6
+tempo_medio_clientes, tempo_medio_atendimento = 1.0 / 2, 1.0 / 1.6
 
 # case 3
-tempo_medio_clientes, tempo_medio_atendimento = 1.0 / 0.5, 1.0 / 0.45
+# tempo_medio_clientes, tempo_medio_atendimento = 1.0 / 0.5, 1.0 / 0.45
 
 # case 4
 # tempo_medio_clientes, tempo_medio_atendimento = 1.0 / 3, 1.0 / 2.97
 
 
 tempo = 0
-tempo_simulacao = 10000
+tempo_simulacao = 50000
 
 
 def minimo(a, b):
@@ -41,15 +45,19 @@ chegada_cliente = (-1.0/tempo_medio_clientes) * log(random())
 # armazena o somatorio do tempo ocupado do caixa
 soma_atendimentos = 0.0
 
-numero_eventos = 0.0
-soma_areas = 0.0
-tempo_anterior = 0.0
+# TODO: Resolver acesso por atributos em en ewE e ewS
+en_numero_eventos = 0.0
+en_soma_areas = 0.0
+en_tempo_anterior = 0.0
 
+ew_numero_eventos_entrada = 0.0
+ew_soma_areas_entrada = 0.0
+ew_tempo_anterior_entrada = 0.0
 
-soma_area_entrada = 0.0
-soma_area_saida = 0.0
-eventos_entrada = 0.0
-eventos_saida = 0.0
+ew_numero_eventos_saida = 0.0
+ew_soma_areas_saida = 0.0
+ew_tempo_anterior_saida = 0.0
+
 
 # armazena o tempo em que o cliente que estiver em atendimento saira do comercio
 saida_atendimento = 0.0
@@ -68,11 +76,10 @@ while tempo < tempo_simulacao:
         tempo = minimo(chegada_cliente, saida_atendimento)
 
     if tempo == chegada_cliente:
-        print(f'Chegada do cliente {chegada_cliente}')
+        logging.debug(f'Chegada do cliente {chegada_cliente}')
         # evento de chegada de cliente
         fila += 1.0
-        eventos_entrada += 1.0
-        print(f'Fila: {fila}')
+        logging.debug(f'Fila: {fila}')
         # indica que o caixa esta ocioso
         # logo, pode-se comecar a atender
         # o cliente que acaba de chegar
@@ -83,12 +90,14 @@ while tempo < tempo_simulacao:
         chegada_cliente = tempo + (-1.0/tempo_medio_clientes) * log(random())
 
         # calculo do E[N]
-        soma_areas += (tempo - tempo_anterior) * numero_eventos
-        tempo_anterior = tempo
-        numero_eventos += 1
+        en_soma_areas += (tempo - en_tempo_anterior) * en_numero_eventos
+        en_tempo_anterior = tempo
+        en_numero_eventos += 1
 
-        soma_area_entrada += (chegada_cliente - tempo_anterior) * eventos_entrada
-
+        # calculo do E[W]
+        ew_soma_areas_entrada += (tempo - ew_tempo_anterior_entrada) * ew_numero_eventos_entrada
+        ew_tempo_anterior_entrada = tempo
+        ew_numero_eventos_entrada += 1
     else:
         # evento de saida de cliente
         # a cabeca da fila nao consiste no cliente em atendimento.
@@ -96,51 +105,54 @@ while tempo < tempo_simulacao:
         # e passa a estar ainda no comercio, mas em atendimento no caixa
         if fila:
             fila -= 1.0
-            eventos_saida += 1.0
-            # tempo_anterior = tempo
             tempo_atendimento = -1.0/tempo_medio_atendimento * log(random())
             saida_atendimento = tempo + tempo_atendimento
 
             soma_atendimentos += tempo_atendimento
 
-            print(f'saida do cliente {saida_atendimento}')
-            print(f'Fila: {fila}')
+            logging.debug(f'saida do cliente {saida_atendimento}')
+            logging.debug(f'Fila: {fila}')
 
         else:
             saida_atendimento = 0.0
 
-        # calculo do E[N]
-        if tempo_anterior < tempo:
-            soma_areas += (tempo - tempo_anterior) * numero_eventos
-            tempo_anterior = tempo
-            numero_eventos -= 1
+        # TODO: Comentar o que esse if faz(reassistir trecho da aula)
+        if en_tempo_anterior < tempo:
+            # calculo do E[N]
+            en_soma_areas += (tempo - en_tempo_anterior) * en_numero_eventos
+            en_tempo_anterior = tempo
+            en_numero_eventos -= 1
 
-        soma_area_saida += (saida_atendimento - tempo) * eventos_saida
+            # calculo do E[W]
+            ew_soma_areas_saida += (tempo - ew_tempo_anterior_saida) * ew_numero_eventos_saida
+            ew_tempo_anterior_saida = tempo
+            ew_numero_eventos_saida += 1
 
-    print('----')
+    logging.debug('----')
 
 # trata problema de tempo de finalização
 # o valor computado na iteração nao condiz com o fim da simulacao
 if saida_atendimento > tempo:
     soma_atendimentos -= (saida_atendimento - tempo)
-    soma_areas -= (saida_atendimento - tempo)
+    # en_soma_areas -= (saida_atendimento - tempo)
 
-# soma_areas += (tempo - tempo_anterior) * numero_eventos
+ew_soma_areas_saida += (tempo - ew_tempo_anterior_saida) * ew_numero_eventos_saida
+ew_soma_areas_entrada += (tempo - ew_tempo_anterior_entrada) * ew_numero_eventos_entrada
 
 U = ((soma_atendimentos / tempo) * 100)
-L = soma_areas/tempo
-W = (soma_area_entrada - soma_area_saida) / eventos_entrada
-_lambda = eventos_entrada/tempo
+EN = en_soma_areas / tempo
+EW = (ew_soma_areas_entrada - ew_soma_areas_saida) / ew_numero_eventos_entrada
+_lambda = ew_numero_eventos_entrada / tempo
 
-# print_mode = 1  # small
-print_mode = 2  # details
+print_mode = 1  # small
+# print_mode = 2  # details
 
 if print_mode == 1:
-    print(f'{U}\t{L}\t{W}\t{_lambda}'.replace('.', ','))
+    print(f'{U}\t{EN}\t{EW}\t{_lambda}'.replace('.', ','))
 else:
     print(f'Taxa de utilização(%): {U:.4f}')
-    print(f'                    L: {L:.4f}')
-    print(f'                    W: {W:.4f}')
+    print(f'                    L: {EN:.4f}')
+    print(f'                    W: {EW:.4f}')
     print(f'               lambda: {_lambda:.4f}')
 
-print(f'Validação de little: {abs(L - _lambda * W)}')
+print(f'Validação de little: {abs(EN - _lambda * EW):.20f}')
